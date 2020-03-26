5356c6de113f0ada2e5be72b4f0f0f32c8b68d10
candeveci13 <mustafacandeveci@gmail.com>
2020-03-26 17:16:17 +0300

    Added jupyter tutorial notes.

--- 1-using-apis.py
+++ 1-using-apis.py
@@ -3,7 +3,8 @@ from tensorflow import keras
 from tensorflow.keras.preprocessing.text import Tokenizer
 
 sentences = ['I love my dog',
-            'I love my cat']
+            'I love my cat',
+             'Ne partez pas sans moi']
 
 tokenizer = Tokenizer(num_words=100)
 tokenizer.fit_on_texts(sentences)
